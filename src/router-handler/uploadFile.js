9ef78cc87bbe12a4d137e72c86dd24bd82c388d7
Cai-Huaibin <100181878+Chb123123@users.noreply.github.com>
2024-04-25 17:31:31 +0800

    新增上传文件功能

--- src/router-handler/uploadFile.js
+++ src/router-handler/uploadFile.js
@@ -6,6 +6,16 @@ exports.uploadImg = (req, res) => {
   var form = new formidable.IncomingForm();
   form.parse(req, function (error, fields, files) {
     if(!files.file) return res.cc('上传的图片不存在')
+    let fileType = files.file.originalFilename.split(".")
+    let type = fileType[fileType.length - 1]
+    let imgType = ["jpg","png","gif","psd","tiff","BMP","eps","svg","jpge",]
+    let flag = imgType.some(item => {
+      if(type === item) {
+        return true
+      }
+    })
+    console.log(flag)
+    if(!flag) return res.cc('上传的图片类型不存在')
     if (error) return res.cc('上传失败')
     var fullFileName = `${+new Date}-${files.file.originalFilename}`;// 拼接图片名称：时间戳-图片名称
     fs.writeFileSync(`${res.path}/src/images/article/${fullFileName}`, fs.readFileSync(files.file.filepath)); // 存储图片到public静态资源文件夹下
@@ -16,3 +26,42 @@ exports.uploadImg = (req, res) => {
     })
   });
 }
+
+/**
+ * @api {post} /my/upload/img 上传图片
+ * @apiName registerUser
+ * @apiGroup upload
+ * 
+ * @apiParam file 上传的文件流
+ * 
+ * @apiSuccess {Number} status 返回的状态码 1 表示成功 0 表示失败
+ * @apiSuccess {String} message 请求说明
+*/
+
+
+// 上传文章图片
+exports.uploadFile = (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (error, fields, files) {
+    if(!files.file) return res.cc('上传的文件不存在')
+    if (error) return res.cc('上传失败')
+    var fullFileName = `${+new Date}-${files.file.originalFilename}`;// 拼接图片名称：时间戳-图片名称
+    fs.writeFileSync(`${res.path}/src/images/files/${fullFileName}`, fs.readFileSync(files.file.filepath)); // 存储图片到public静态资源文件夹下
+    res.send({
+      status: 1,
+      message: '上传成功',
+      location: `${res.website}/images/files/${fullFileName}`  // 图片网络地址
+    })
+  });
+}
+
+/**
+ * @api {post} /my/upload/file 上传文件
+ * @apiName registerUser
+ * @apiGroup upload
+ * 
+ * @apiParam file 上传的文件流
+ * 
+ * @apiSuccess {Number} status 返回的状态码 1 表示成功 0 表示失败
+ * @apiSuccess {String} message 请求说明
+*/
--- src/router/uploadFile.js
+++ src/router/uploadFile.js
@@ -5,4 +5,6 @@ const upload = require('../router-handler/uploadFile.js')
 
 // 上传图片功能
 router.post('/upload/img', upload.uploadImg)
+// 上传文件功能
+router.post('/upload/file', upload.uploadFile)
 module.exports = router
